7c813f890177396443942c2f816d2911078fb2be
autofillmode <axu.hartikainen@gmail.com>
2025-04-21 15:54:16 +0300

    fix: rewrite example_types to actually work

--- examples/example_types.c
+++ examples/example_types.c
@@ -4,13 +4,18 @@
 int
 main (void)
 {
-  Hash_Table *ht = ht_init (STRING, FLOAT);
+  Hash_Table *ht = ht_init (STRING, ANY);
 
   float points = 3.14;
   float *np = &points;
   ht_put ("points", np, ht);
 
+  int another = 774;
+  int *ap = &another;
+  ht_put ("another", ap, ht);
+
   printf ("points: %0.2f\n", *(float *)ht_get ("points", ht));
+  printf ("another: %d\n", *(int *)ht_get ("another", ht));
 
   ht_free (ht);
 }
